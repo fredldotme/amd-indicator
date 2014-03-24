4596806d4f0b4d6853e73e902c963f738f4bab61
Sadi Yumuşak <sadiyumusak@gmail.com>
2014-03-24 16:02:12 +0200

    Major Update

--- setup.sh
+++ setup.sh
@@ -4,30 +4,38 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 else
 	rm -f /etc/ld.so.conf.d/multiarchfix.conf
-	mkdir -p /usr/lib/amdindicator
 	mkdir -p /usr/i386-linux-gnu
-	cp amd-indicator /usr/bin/
-	chown root:root /usr/bin/amd-indicator
-	chmod 755 /usr/bin/amd-indicator
-
 	if ! [[ -L /usr/i386-linux-gnu/mesa ]]
 	then
 		ln -s /usr/lib/i386-linux-gnu/mesa /usr/i386-linux-gnu/mesa
 	fi
 
-	cp igpuon /usr/lib/amdindicator/
-	cp dgpuon /usr/lib/amdindicator/
-	cp intel.png /usr/lib/amdindicator/
-	cp amd.png /usr/lib/amdindicator/
-	chmod a+r /usr/lib/amdindicator/*.png
-	chown root:root /usr/lib/amdindicator/igpuon
-	chown root:root /usr/lib/amdindicator/dgpuon
-	chmod a+x /usr/lib/amdindicator/igpuon
-	chmod a+x /usr/lib/amdindicator/dgpuon
-	cp amd-indicator-sudoers /etc/sudoers.d/
-	chmod 644 /etc/sudoers.d/amd-indicator-sudoers
+	rm -f /usr/bin/amd-indicator
+	rm -Rf /usr/lib/amdindicator
+	rm -Rf /usr/local/indicator-amd
 
-	read -n1 -p "Autostart AMD Indicator? (y/N) "
-	echo
-	[[ $REPLY = [yY] ]] && cp amd-indicator.desktop $HOME/.config/autostart/ || { rm $HOME/.config/autostart/amd-indicator.desktop; }
-fi
+	mkdir -p /usr/local/indicator-amd
+	cp amd-indicator /usr/local/indicator-amd/
+	chown root:root /usr/local/indicator-amd/amd-indicator
+	chmod 755 /usr/local/indicator-amd/amd-indicator
+	ln -sf /usr/local/indicator-amd/amd-indicator /usr/local/bin/amd-indicator
+	cp dgpuon /usr/local/indicator-amd/
+	chown root:root /usr/local/indicator-amd/dgpuon
+	chmod a+x /usr/local/indicator-amd/dgpuon
+	cp igpuon /usr/local/indicator-amd/
+	chown root:root /usr/local/indicator-amd/igpuon
+	chmod a+x /usr/local/indicator-amd/igpuon
+	cp restart /usr/local/indicator-amd/
+	chown root:root /usr/local/indicator-amd/restart
+	chmod a+x /usr/local/indicator-amd/restart
+	cp amd.png /usr/local/indicator-amd/
+	cp amd-dark.png /usr/local/indicator-amd/
+	cp amd-light.png /usr/local/indicator-amd/
+	cp intel.png /usr/local/indicator-amd/
+	cp intel-dark.png /usr/local/indicator-amd/
+	cp intel-light.png /usr/local/indicator-amd/
+	chmod a+r /usr/local/indicator-amd/*.png
+	cp -f amd-indicator-sudoers /etc/sudoers.d/
+	chmod 644 /etc/sudoers.d/amd-indicator-sudoers
+	cp -f amd-indicator.desktop /etc/xdg/autostart/
+	cp -f amd-indicator.desktop /usr/share/applications/
